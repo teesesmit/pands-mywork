450e7f1830f9ad7d133021a9929965197832836e
theresa smyth <teesesmit@hotmail.com>
2024-03-12 16:18:13 +0000

    Created lab3.1.6 extra questions

---
+++ week03/Lab3.1.6. Extra Questions.py
@@ -0,0 +1,17 @@
+# Lab3.1 Variable and State
+# Extra questions
+# Author: Theresa Smyth
+
+# Why does this expression cause an error? How can you fix it?
+# answer: number needs to be converted into string
+
+# Why is eggs a valid variable name while 100 is invalid?
+# answer: 100 is a number, variable names cannot begin with a number
+
+# What three functions can be used to get the integer, floating-point number, 
+# or string version of a value?
+# answer: int(), float(), str()
+
+message = 'I have eaten ' + str(99) + ' burritos.'
+print (message)
+
